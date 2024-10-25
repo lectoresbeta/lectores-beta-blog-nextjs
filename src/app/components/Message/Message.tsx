c6d320c011a50b2c8b4a46d530f2c807127dd966
pablo <web@pablanco.com>
2024-10-25 23:44:26 +0200

    FIx errors 3

--- src/app/components/Message/Message.tsx
+++ src/app/components/Message/Message.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import './Message.scss';
 import { CheckCircleOutlined } from '@ant-design/icons';
 
-const Message = ({ children, className = '' }) => {
+interface MessageProps {
+  children: ReactNode;
+  className?: string;
+}
+
+const Message: React.FC<MessageProps> = ({ children, className = '' }) => {
   return (
-    <div className={`c__Message ${className}`} >
+    <div className={`c__Message ${className}`}>
       <CheckCircleOutlined />
       <span>{children}</span>
     </div>
