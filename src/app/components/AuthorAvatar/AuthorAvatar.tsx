e574d8333cd41e46e76d72c2b4cc7f3fa1228206
pablo <web@pablanco.com>
2024-10-25 23:25:59 +0200

    Fix errors 8

--- src/app/components/AuthorAvatar/AuthorAvatar.tsx
+++ src/app/components/AuthorAvatar/AuthorAvatar.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import { Avatar } from 'antd';
 import "./AuthorAvatar.scss";
 
-const AuthorAvatar = (imageUrl : string) => {
+interface AuthorAvatarProps {
+  imageUrl: string;
+}
+
+const AuthorAvatar: React.FC<AuthorAvatarProps> = ({ imageUrl }) => {
   return (
     <div className='c__AuthorAvatar'>
       <Avatar src={imageUrl} size="large" />
