2cf88219b6a3e9bfbdd509296a54692cea031379
pablo <web@pablanco.com>
2024-10-26 00:22:58 +0200

    Add google anaytics and google fonts

---
+++ src/app/components/GoogleAnalytics/GoogleAnalytics.tsx
@@ -0,0 +1,25 @@
+import { useEffect } from 'react';
+import Script from 'next/script';
+
+const GoogleAnalytics = () => {
+  useEffect(() => {
+    window.dataLayer = window.dataLayer || [];
+    function gtag(...args: any) {
+      window.dataLayer.push(args);
+    }
+    gtag('js', new Date());
+    gtag('config', 'G-KKRZLDYB14');
+  }, []);
+
+  return (
+    <>
+      {/* Carga el script de Google Analytics */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-KKRZLDYB14"
+        strategy="afterInteractive"
+      />
+    </>
+  );
+};
+
+export default GoogleAnalytics;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import CookieConsent from './components/CookieConsent/CookieConsent';
 import './styles.scss';
+import GoogleAnalytics from './components/GoogleAnalytics/GoogleAnalytics';
 
 export default function RootLayout({
   children,
@@ -17,6 +18,7 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Lectores Beta</title>
+        <GoogleAnalytics />
       </head>
       <body>
         <div>
